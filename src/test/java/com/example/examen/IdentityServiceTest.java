e9adf27985c7b008e6143f75c96de1bf25ff3a68
987sebasBeller <sebastianbeller76@gmail.com>
2024-10-01 18:55:43 -0400

    .

--- src/test/java/com/example/examen/IdentityServiceTest.java
+++ src/test/java/com/example/examen/IdentityServiceTest.java
@@ -10,13 +10,21 @@ class IdentityServiceTest {
     @Test
     @DisplayName("Verify an existent ci")
     void verifyExistence() {
+        //arrange
         IdentityService identityService = new IdentityService();
-        assertEquals(identityService.verifyExistence(1), true);
+        //act
+        boolean response=identityService.verifyExistence(1);
+        //ASSERT
+        assertEquals(response, true);
     }
     @Test
     @DisplayName("Verify an not existent ci")
     void verifyUnExistence() {
+        //arrange
         IdentityService identityService = new IdentityService();
-        assertEquals(identityService.verifyExistence(5), false);
+        //act
+        boolean response=identityService.verifyExistence(10);
+        //ASSERT
+        assertEquals(response, false);
     }
 }
